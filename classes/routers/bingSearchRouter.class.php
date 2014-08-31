b1c3ef74502a08204fc4ee3d7949e33be576b3a7
Cristian Ronzio <cristian.ronzio@gmail.com>
2014-08-31 22:45:06 +0200

    remote_search_url and params

--- classes/routers/bingSearchRouter.class.php
+++ classes/routers/bingSearchRouter.class.php
@@ -15,6 +15,12 @@
       */
      public $results = array();
      
+     /**
+      *
+      * @var int @TODO
+      */
+     public $skip = null;
+     
      public function __construct($search_query = "")
      {
          parent::__construct($search_query);         
@@ -36,12 +42,29 @@
      {
          $router = $this->get_router_name();
          $this->apikey = $this->plugin->config['search_providers'][$router]['API_KEY'];
+         $this->set_remote_search_url_params();
+     }
+     
+     private function set_remote_search_url_params()
+     {
+         $this->remote_search_url .= "&Query='" . urlencode(urldecode($this->search_query));
+         $domain = 'site:' . $this->context_domain;
+
+         $skip = ($this->skip > 0) ? '&$skip=' . $this->skip : "";
+         $this->remote_search_url .= urlencode(" $domain'") . $skip;
+         if(
+             $this->plugin->config['search_providers'][$this->router_name]['highlight_title'] || 
+             $this->plugin->config['search_providers'][$this->router_name]['highlight_title']
+             ) {
+             $this->remote_search_url .= "&Options='EnableHighlighting'";
+         }
+         
      }
      
      protected function get_remote_results()
      {
          $ch = curl_init();
-         curl_setopt($ch, CURLOPT_URL, $this->search_uri);
+         curl_setopt($ch, CURLOPT_URL, $this->remote_search_url);
          curl_setopt($ch, CURLOPT_HEADER, false);
          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
          curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
